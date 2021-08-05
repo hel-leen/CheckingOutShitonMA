4a6e6dfd6971e27a3be97aa3383a7c71e7dd20af
hel-leen <79870754+hel-leen@users.noreply.github.com>
2021-08-05 19:37:35 +0900

    updating

--- html/top.init.js
+++ html/top.init.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 	window.matchMedia( '(max-width: 767px)' ).matches &&
 	navigator.userAgent.search(/mobile/gi) < 0 ?
       $( ":root" ).css( "font-size","100%" ) :
-      $( ":root" ).css( "font-size","2.3vh" )
+      $( ":root" ).css( "font-size","2vh" )
 });
 $(function() {
   $('.toplist').DataTable({
