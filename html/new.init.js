6dd5735a927e2f3f970265e0ead2e59983ba4d64
hel-leen <79870754+hel-leen@users.noreply.github.com>
2021-10-08 03:57:32 +0900

    updating

--- html/new.init.js
+++ html/new.init.js
@@ -422,8 +422,7 @@ $(function () {
       // time serials 
       api.columns(8).every(function () {
         var
-          column = this,
-          frames = [],
+          column = this, frames = [], xrange,
           date =
             Object.entries(this.data().map((d, j) => { return d = d.split('|||')[0]; }).reduce(function (obj, item) { obj[item] = (obj[item] || 0) + 1; return obj; }, {}))
               .filter(entry => {
@@ -431,39 +430,31 @@ $(function () {
                 if (datevalue < (moment().valueOf() + days / 2) & datevalue > (moment().valueOf() - days))
                   return entry
               });
-        dates = date.map(entry => { return entry = entry[0] }),
-          dateCount = date.map(entry => { return entry = entry[1]; }),
-          texts = dates.map(item => { item == thisday ? item = 'Today' : item = ''; return item })
-        var x = dates;
-        var y = dateCount;
-        var xrange;
+          x = date.map(entry => { return entry = entry[0] }),
+          y = date.map(entry => { return entry = entry[1]; }),
+          texts = x.map(item => { item == thisday ? item = 'Today' : item = ''; return item })
         for (var i = 0; i <= y.length; i++) {
           frames[i] = {
-            data: [
-              { x: [], y: [], fillcolor: '' },
-              { x: [], y: [], text: '' },
-              { x: [], fillcolor: '' },
-            ]
+            data: [ { x: [], y: [], fillcolor: '' }, { x: [], y: [], text: '' } ]
             , layout: {
               // xaxis: { range: [] } ,
               shapes: []
             },
           };
-          frames[i].data[0].x = dates.slice(0, i + 1); frames[i].data[0].y = dateCount.slice(0, i + 1);
-          frames[i].data[1].x = dates.slice(0, i + 1); frames[i].data[1].y = dateCount.slice(0, i + 1).map(count => { return count = count + 10 });
+          frames[i].data[0].x = frames[i].data[1].x  = x.slice(0, i + 1); 
+		  frames[i].data[0].y = y.slice(0, i + 1);
+          frames[i].data[1].y = y.slice(0, i + 1).map(count => { return count = count + 10 });
           var colorfill = 'hsla('.concat(330 - ((i + 1) * 330 / y.length) + 10), alphafill = Math.abs(Math.sin(Math.floor((i + 50) / 50))) / 100 + 0.085;
-          // (i == y.length) ? colorfill+= ',1,1,'.concat(alphafill,')'): 
           colorfill += ',.99,.9,'.concat(alphafill, ')');
-          xrange = dates.slice(0, i + 1).slice(-1)[0];
+          xrange = x.slice(0, i + 1).slice(-1)[0];
+          frames[i].data[0].fillcolor = colorfill;
           i == y.length ? frames[i].data[1].text = texts.slice(0, i + 1) : '';
           if (thisweek <= xrange)
             frames[i].layout.shapes = [{
-              x0: thisweek, y0: 0, x1: xrange, y1: 1,
-              type: 'rect', xref: 'x', yref: 'paper', fillcolor: 'rgba(222,222,222,.2)', opacity: .1, line: { width: 0 }
+              x0: thisweek, y0: 0, x1: xrange, y1: 1,type: 'rect', xref: 'x', yref: 'paper', fillcolor: 'rgba(222,222,222,.2)', opacity: .1, line: { width: 0 }
             }]
-          frames[i].data[0].fillcolor = colorfill;
         }
-        // console.log(dates );
+        // console.log(x );
         var data = [{
           name: "line", x: frames[0].data[0].x, y: frames[0].data[0].y,
           type: "scatter", mode: "lines+text", fill: 'tozeroy', fillcolor: 'rgba(238, 221, 204,.5)', line: { color: 'rgba(116,111,111,.8)', width: 1 },
@@ -475,7 +466,7 @@ $(function () {
           type: "scatter", mode: "text",hoverinfo:'skip',
         }];
         var layout = {
-          height: 160, margin: { t: 0, r: 10, b: 20, l: 10 }, showlegend: false,
+          height: 160, margin: { t: 0, r: 10, b: 22, l: 10 }, showlegend: false,
           paper_bgcolor: "rgba(0,0,0,0)", plot_bgcolor: "rgba(0,0,0,0)", font: { color: 'rgba(238,221,204,.7)', },
           xaxis: {
             automargin: false, fixedrange: true, showgrid: false,
@@ -484,7 +475,7 @@ $(function () {
             range: [frames.slice(-1)[0].data[0].x[0], frames.slice(-1)[0].data[0].x.slice(-1)[0]],
           },
           yaxis: {
-            range: [0, (Math.floor(Math.max(...dateCount) / 10) + 3) * 10], gridcolor: 'rgba(111,111,111,.2)',
+            range: [0, (Math.floor(Math.max(...y) / 10) + 3) * 10], gridcolor: 'rgba(111,111,111,.2)',
             showgrid: true, zeroline: false, showline: false, autotick: true, ticks: '', fixedrange: true, showticklabels: false
           },
         };
@@ -501,12 +492,10 @@ $(function () {
             var val = $.fn.dataTable.util.escapeRegex(data.points[i].x);
             if (val != '') {
               $('body,html').animate({ scrollTop: $('.dataTables tbody').offset().top - $(".dataTables_filter").height() - 8, }, 800);
-              $('.filter-holder.8 input').val('');
-              column.search(val ? val : '', true, true).draw();
+              $('.filter-holder.8 input').val(''); column.search(val ? val : '', true, true).draw();
             }
           }
         });
-
       });
       if (localStorage.getItem('NewSelected') != undefined) {
         var selected = localStorage.getItem('NewSelected').split(',').join('|');
